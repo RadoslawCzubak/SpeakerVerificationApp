cbffb8810a6c16dafcf1dd0898f9cff2a5c00f3a
Radosław Czubak <radoslaw.marek.czubak@gmail.com>
2022-12-29 15:01:15 +0100

    add improved recorder

--- ui/RecordWindow.py
+++ ui/RecordWindow.py
@@ -1,7 +1,9 @@
 from PySide6.QtCore import Slot
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton
 from PySide6.QtWidgets import QMainWindow
-from voicerecorder import recorder
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton
+
+from voicerecorder.recorder import Recorder
+
 
 class RecordWindow(QMainWindow):
     def __init__(self):
@@ -13,7 +15,34 @@ class RecordWindow(QMainWindow):
         layout.addWidget(self.record_button)
         window.setLayout(layout)
         self.setCentralWidget(window)
+        self.recorder = None
+        self.is_recording = False
+        self.set_recorder_state(False)
 
     @Slot()
     def record_audio(self):
-        recorder.record_audio("test.wav", seconds=5)
+        if self.recorder is None:
+            self._start_record()
+        else:
+            self._stop_record()
+
+    def _start_record(self):
+        def on_recorder_stop():
+            self.set_recorder_state(False)
+            self.recorder = None
+
+        self.set_recorder_state(True)
+        self.recorder = Recorder(output_path="test.wav")
+        self.recorder.record_audio()
+        self.recorder.set_on_stop_listener(on_recorder_stop)
+
+    def _stop_record(self):
+        self.set_recorder_state(False)
+        self.recorder.stop()
+        self.recorder = None
+
+    def set_recorder_state(self, is_enabled: bool):
+        if is_enabled:
+            self.record_button.setText("Stop")
+        else:
+            self.record_button.setText("Record")
